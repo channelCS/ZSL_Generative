73e901d698a8130d228d08a7d32c7efd1874f45c
animesh-007 <animeshagarwal7@gmail.com>
2021-06-26 20:04:55 +0530

    changed v1 download script

--- datasets/v1/download.sh
+++ datasets/v1/download.sh
@@ -1,34 +1,5 @@
-echo "==> Downoading datasets"
-# CUB dataset
-echo "==> Downoading CUB dataset zip file"
-gdown --id 1pTdH0TSGeuwJY0gf_kl64TjQVeN0fnZd
-echo "==> Unzipping CUB dataset zip file"
-unzip CUB-20210425T113813Z-001.zip
-echo "==> Removing CUB dataset zip file"
-rm CUB-20210425T113813Z-001.zip
-
-# AWA2 dataset
-echo "==> Downoading AWA2 dataset zip file"
-gdown --id 1FEJWB5goZEQv4pTAMrJTRzJ-RPpBirzk
-echo "==> Unzipping AWA2 dataset zip file"
-unzip AWA2-20210614T103407Z-001.zip
-echo "==> Removing AWA2 dataset zip file"
-rm AWA2-20210614T103407Z-001.zip
-
-# FLO dataset
-echo "==> Downoading FLO dataset zip file"
-gdown --id 1tJ5pFJaylnHGtajkb3FPwFtVjAx7xs3u
-echo "==> Unzipping FLO dataset zip file"
-unzip FLO-20210614T101759Z-001.zip
-echo "==> Removing FLO dataset zip file"
-rm FLO-20210614T101759Z-001.zip
-
-# SUN dataset
-echo "==> Downoading SUN dataset zip file"
-gdown --id 1IYAGZCq5ghhnmo_2wgvmLW8IEcUNk34X
-echo "==> Unzipping SUN dataset zip file"
-unzip SUN-20210614T150751Z-001.zip
-echo "==> Removing SUN dataset zip file"
-rm SUN-20210614T150751Z-001.zip
-
-echo "==> Downoading datasets completed"
+echo "==> Downloading version 1 of the datasets"
+gdown --id 1S8MpdUoNGZlvmmoyF_DNR1hA676SNhvP
+unzip v1.zip
+rm v1.zip
+echo "==> ==> Downoading version 1 of the datasets completed"
